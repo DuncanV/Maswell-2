cc3f688e2c084a6604adbf516fe0bc22afb5367f
DuncanV <duncanvodden999@gmail.com>
2018-10-10 11:41:40 +0200

    added Doxygen

--- Project/ConcreteRegistrationManager.h
+++ Project/ConcreteRegistrationManager.h
@@ -1,6 +1,10 @@
-//
-// Created by  Duncan + Tjaart on 2018/10/10.
-//
+/**
+ *  @file ConcreteRegistrationManager.h
+ *  @class ConcreteRegistrationManager
+ *  @authors Duncan + Tjaart
+ *  @version 1.0.0
+ *  @brief ConcreteMediator for mediator design pattern
+ */
 
 #ifndef PROJECT_CONCRETEREGISTRATIONMANAGER_H
 #define PROJECT_CONCRETEREGISTRATIONMANAGER_H
@@ -10,15 +14,49 @@
 class ConcreteRegistrationManager:public RegistratcionManager
 {
 public:
+    /**
+     * constructor for ConcreteRegistrationManager
+     * @param _numTracks determines how many racetracks there are
+     */
     ConcreteRegistrationManager(int _numTracks);
+
+    /**
+     * destructor for manager
+     */
     ~ConcreteRegistrationManager();
+
+    /**
+     * implementation of absrtact function to add car into the cars array
+     * @param _car car object to be placed in the array
+     * @param track specifies which track the car will be racing
+     */
     virtual void addCar(Car* _car, int track);
 
 private:
+    /**
+    * the number of tracks the mediator looks after
+    */
     int numTracks;
+
+    /**
+     * the amount of cars on each track
+     */
     int* numCars;
+
+    /**
+     * the size/ Max capacity of the track, dynamic
+     */
     int* size;
+
+    /**
+     * cars array holds tracks and the cars
+     */
     Car*** cars;
+
+    /**
+     * resizes the track number of cars to fit more cars in
+     * @param trackNo the track no to be resized
+     */
     void resize(int trackNo);
 
 };
--- Project/RegistrationManager.h
+++ Project/RegistrationManager.h
@@ -1,6 +1,11 @@
-//
-// Created by  Duncan + Tjaart on 2018/10/10.
-//
+/**
+ *  @file RegistratcionManager.h
+ *  @class ConcreteRegistrationManager
+ *  @authors Duncan + Tjaart
+ *  @version 1.0.0
+ *  @brief AbstractMediator for mediator design pattern
+ */
+
 
 #ifndef PROJECT_REGISTRATIONMANAGER_H
 #define PROJECT_REGISTRATIONMANAGER_H
@@ -10,6 +15,9 @@ using namespace std;
 class Car;
 class RegistratcionManager{
 public:
+    /**
+    * abstract function to add cars into mediator
+    */
     virtual void addCar(Car* _car, int track)=0;
 private:
 
